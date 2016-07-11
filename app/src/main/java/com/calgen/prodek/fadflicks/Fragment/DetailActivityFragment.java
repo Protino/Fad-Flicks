c3cbfee82ccff81c209bbe06b84d7b1618361976
Protino <gurupadmamadapur@gmail.com>
2016-07-11 20:56:59 +0530

    Add DetailActivity
    Implement material design to the activity_detail
    layout by using co-ordinator layout and scrolling
    flags. Also adjust views in content_detail such that size of
    movie poster won't harm other view's layout bounds.

---
+++ app/src/main/java/com/calgen/prodek/fadflicks/Fragment/DetailActivityFragment.java
@@ -0,0 +1,24 @@
+package com.calgen.prodek.fadflicks.Fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.calgen.prodek.fadflicks.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailActivityFragment extends Fragment {
+
+    public DetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
