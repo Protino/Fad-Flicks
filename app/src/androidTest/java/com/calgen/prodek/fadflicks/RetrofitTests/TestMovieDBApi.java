c75a6f46573c403e96c90d2119bb34a38222d662
Protino <gurupadmamadapur@gmail.com>
2016-09-02 12:58:45 +0530

    Add test cases for other endpoints

--- app/src/androidTest/java/com/calgen/prodek/fadflicks/RetrofitTests/TestMovieDBApi.java
+++ app/src/androidTest/java/com/calgen/prodek/fadflicks/RetrofitTests/TestMovieDBApi.java
@@ -4,7 +4,11 @@ import android.test.AndroidTestCase;
 
 import com.calgen.prodek.fadflicks.Utility.ApplicationConstants;
 import com.calgen.prodek.fadflicks.api.ApiClient;
+import com.calgen.prodek.fadflicks.model.Credits;
+import com.calgen.prodek.fadflicks.model.Genres;
 import com.calgen.prodek.fadflicks.model.MovieResponse;
+import com.calgen.prodek.fadflicks.model.ReviewResponse;
+import com.calgen.prodek.fadflicks.model.VideoResponse;
 
 import java.io.IOException;
 
@@ -26,17 +30,51 @@ public class TestMovieDBApi extends AndroidTestCase {
 
     public void testMoviesEndpoint() throws IOException {
 
-        Call<MovieResponse> topRatedList = apiClient.movieInterface().getMovies(TestData.SORT_BY, TestData.VOTE_COUNt);
-        Response<MovieResponse> movieResponse = topRatedList.execute();
+        Call<MovieResponse> call = apiClient.movieInterface().getMovies(TestData.SORT_BY, TestData.VOTE_COUNt);
+        Response<MovieResponse> response = call.execute();
 
-        assertEquals(movieResponse.code(), 200);
-        assertEquals(true, movieResponse.isSuccessful());
+        assertEquals(response.code(), 200);
+        assertEquals(true, response.isSuccessful());
 
+    }
+
+    public void testGenresEndpoint() throws IOException {
+        Call<Genres> call = apiClient.movieInterface().getGenres();
+        Response<Genres> response = call.execute();
+
+        assertEquals(response.code(), 200);
+        assertEquals(true, response.isSuccessful());
+    }
+
+    public void testVideosEndpoint() throws IOException {
+        Call<VideoResponse> call = apiClient.movieInterface().getVideos(TestData.MOVIE_ID);
+        Response<VideoResponse> response = call.execute();
+
+        assertEquals(response.code(), 200);
+        assertEquals(true, response.isSuccessful());
+
+    }
+
+    public void testReviewsEndpoint() throws IOException {
+        Call<ReviewResponse> call = apiClient.movieInterface().getReviews(TestData.MOVIE_ID);
+        Response<ReviewResponse> response = call.execute();
+
+        assertEquals(response.code(), 200);
+        assertEquals(true, response.isSuccessful());
+    }
+
+    public void testCreditsEndpoint() throws IOException {
+        Call<Credits> call = apiClient.movieInterface().getCredits(TestData.MOVIE_ID);
+        Response<Credits> response = call.execute();
+
+        assertEquals(response.code(), 200);
+        assertEquals(true, response.isSuccessful());
     }
 
 
     public static class TestData {
         public static final String SORT_BY = "popularity.desc";
         public static final int VOTE_COUNt = 1000;
+        public static final int MOVIE_ID = 209112; // Dark Knight Rises
     }
 }
--- app/src/main/java/com/calgen/prodek/fadflicks/api/ApiInterface.java
+++ app/src/main/java/com/calgen/prodek/fadflicks/api/ApiInterface.java
@@ -1,12 +1,31 @@
 package com.calgen.prodek.fadflicks.api;
 
+import com.calgen.prodek.fadflicks.model.Credits;
+import com.calgen.prodek.fadflicks.model.Genres;
 import com.calgen.prodek.fadflicks.model.MovieResponse;
+import com.calgen.prodek.fadflicks.model.ReviewResponse;
+import com.calgen.prodek.fadflicks.model.VideoResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
+
+
+    @GET("genre/movie/list")
+    Call<Genres> getGenres();
+
+    @GET("movie/{id}/credits")
+    Call<Credits> getCredits(@Path("id") int movieId);
+
+    @GET("movie/{id}/videos")
+    Call<VideoResponse> getVideos(@Path("id") int movieId);
+
+    @GET("movie/{id}/reviews")
+    Call<ReviewResponse> getReviews(@Path("id") int movieId);
+
     @GET("discover/movie")
     Call<MovieResponse> getMovies(@Query("sort_by") String popularity, @Query("vote_count.gte") int voteCount);
 }
